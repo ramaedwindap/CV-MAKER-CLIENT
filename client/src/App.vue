dc0742567f59a63318f46a116b8d4734367ffec0
Rama Edwinda Putra <ramaedwindap@gmail.com>
2023-08-30 19:19:07 +0700

    feat: add CustomNavbar and install heroicons

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,16 @@
 <template>
+  <CustomNavbar />
   <RouterView />
 </template>
 
 <script>
 import { RouterLink, RouterView } from "vue-router";
+import CustomNavbar from "./components/CustomNavbar.vue";
+
+export default {
+  data() {
+    return {};
+  },
+  components: { CustomNavbar },
+};
 </script>
